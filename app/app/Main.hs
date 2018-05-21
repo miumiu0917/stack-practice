0011a11a099cacfcc88b0e6f6a02dfc07842741a
miumiu0917 <miumiu917@gmail.com>
2018-05-22 08:16:51 +0900

    減算の実装

--- app/app/Main.hs
+++ app/app/Main.hs
@@ -4,4 +4,4 @@ import Pfad
 import SpaceVector
 
 main :: IO ()
-main = print $ Vector 3 2 7 `dot` Vector 2 2 2 
+main = print $ Vector 3 2 7 `minus` Vector 2 2 2 
--- app/src/SpaceVector.hs
+++ app/src/SpaceVector.hs
@@ -3,6 +3,7 @@ module SpaceVector
     , plus
     , multiply
     , dot
+    , minus
     ) where
 
 data Vector a = Vector a a a deriving (Show)
@@ -15,3 +16,8 @@ a `multiply` Vector x1 y1 z1 = Vector (a * x1) (a * y1) (a * z1)
 
 dot :: (Num a) => Vector a -> Vector a -> a
 Vector x1 y1 z1 `dot` Vector x2 y2 z2 = (x1 * x2) + (y1 * y2) + (z1 * z2)
+
+minus :: (Num a) => Vector a -> Vector a -> Vector a
+Vector x1 y1 z1 `minus` Vector x2 y2 z2 = v1 `plus` ((-1) `multiply` v2)
+  where v1 = Vector x1 y1 z1
+        v2 = Vector x2 y2 z2
