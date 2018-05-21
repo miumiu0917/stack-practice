59967721e93a496f29a7f4a7ee1e52150bbf9f1e
miumiu0917 <miumiu917@gmail.com>
2018-05-22 07:57:51 +0900

    内積の実装

--- app/src/SpaceVector.hs
+++ app/src/SpaceVector.hs
@@ -14,4 +14,4 @@ multiply :: (Num a) => a -> Vector a -> Vector a
 a `multiply` Vector x1 y1 z1 = Vector (a * x1) (a * y1) (a * z1)
 
 dot :: (Num a) => Vector a -> Vector a -> a
-Vector x1 y1 z1 `dot` Vector x2 y2 z2 = (x1 * x2) + (y1 + y2) + (z1 + z2)
+Vector x1 y1 z1 `dot` Vector x2 y2 z2 = (x1 * x2) + (y1 * y2) + (z1 * z2)
